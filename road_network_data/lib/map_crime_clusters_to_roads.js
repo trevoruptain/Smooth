584bd0fd79aae0a8670a1d84d35e7bc65836cfd4
Alvin Kong <alvink92@gmail.com>
2017-10-10 20:07:24 -0700

    .

--- road_network_data/lib/map_crime_clusters_to_roads.js
+++ road_network_data/lib/map_crime_clusters_to_roads.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const crimeRoadsUtil = require("./crime_clusters_to_roads_util");
 
-const CRIME_RADIUS = 80; // in meters
 const processedRoadsExportPath = path.join(
   __dirname,
   "../data/processed/",
@@ -20,40 +20,42 @@ const intersections = require(path.join(
   "../data/processed/",
   "intersections.json"
 ));
+const intersectionsById = crimeRoadsUtil.intersectionsById(intersections);
 
-const latToMeters = lat => {
-  return 110992.18 * lat;
-};
+let crimeTotal = 0;
 
-const lngToMeters = lng => {
-  return 88101.33 * lng;
-};
-
-const distance = (pos1, pos2) => {
-  return Math.sqrt(Math.pow(pos2.x - pos1.x, 2) + Math.pow(pos2.y - pos1.y, 2));
-};
-
-const createBounds = (pos1, pos2) => {
-  const isInBound = (x, y) => {
-    return (
-      (x >= Math.min(pos1.x, pos2.x) && y >= Math.min(pos1.y, pos2.y)) &&
-      (x <= Math.max(pos1.x, pos2.x) && y <= Math.max(pos1.y, pos2.y))
-    );
-  };
-  return isInBound;
-};
-
-roads.slice(0,2).forEach((road, i) => {
+roads.forEach((road, i) => {
+  if (i % 10000 === 0) console.log(i);
   const roadCrimes = [];
-  const i1Pos = {
-    x: lngToMeters(intersections[road.intersection1_id].location.lng),
-    y: latToMeters(intersections[road.intersection1_id].location.lat)
-  };
-  const i2Pos = {
-    x: lngToMeters(intersections[road.intersection2_id].location.lng),
-    y: latToMeters(intersections[road.intersection2_id].location.lat)
-  };
-  const isInBounds = createBounds(i1Pos, i2Pos);
+  let i1Pos;
+  let i2Pos;
+
+  try {
+    i1Pos = {
+      x: crimeRoadsUtil.lngToMeters(
+        intersectionsById[road.intersection1_id].location.lng
+      ),
+      y: crimeRoadsUtil.latToMeters(
+        intersectionsById[road.intersection1_id].location.lat
+      )
+    };
+    i2Pos = {
+      x: crimeRoadsUtil.lngToMeters(
+        intersectionsById[road.intersection2_id].location.lng
+      ),
+      y: crimeRoadsUtil.latToMeters(
+        intersectionsById[road.intersection2_id].location.lat
+      )
+    };
+  } catch (e) {
+    // statements to handle any exceptions
+    console.log(`error`, road);
+    roads[i]["crimes"] = [];
+    roads[i]["totalCrimeRating"] = 0;
+    return;
+  }
+
+  const isInBounds = crimeRoadsUtil.createBounds(i1Pos, i2Pos);
 
   const m = // slope for intersection 1 to intersection 2
     (i2Pos.y - i1Pos.y) / (i2Pos.x - i1Pos.x);
@@ -62,32 +64,44 @@ roads.slice(0,2).forEach((road, i) => {
 
   crimes.forEach((crime, j) => {
     const crimePos = {
-      x: lngToMeters(crime.location.lng),
-      y: latToMeters(crime.location.lat)
+      x: crimeRoadsUtil.lngToMeters(crime.location.lng),
+      y: crimeRoadsUtil.latToMeters(crime.location.lat)
     };
     const b_c = -n * crimePos.x + crimePos.y;
 
     const xOrthIntersection = (b_c - b_p) / (m - n);
     const yOrthIntersection = m * xOrthIntersection + b_p;
-    const orthIntersectionPos = {x: xOrthIntersection, y: yOrthIntersection};
+    const orthIntersectionPos = { x: xOrthIntersection, y: yOrthIntersection };
 
     if (isInBounds(xOrthIntersection, yOrthIntersection)) {
-      if (distance(crimePos, orthIntersectionPos) < CRIME_RADIUS ){
+      if (
+        crimeRoadsUtil.distance(crimePos, orthIntersectionPos) <
+        crimeRoadsUtil.CRIME_RADIUS
+      ) {
         roadCrimes.push(crime);
       }
     } else {
-      const d1 = distance(crimePos, i1Pos);
-      const d2 = distance(crimePos, i2Pos);
+      const d1 = crimeRoadsUtil.distance(crimePos, i1Pos);
+      const d2 = crimeRoadsUtil.distance(crimePos, i2Pos);
 
-      if (d1 <= CRIME_RADIUS || d2 <= CRIME_RADIUS) roadCrimes.push(crime);
+      if (
+        d1 <= crimeRoadsUtil.CRIME_RADIUS ||
+        d2 <= crimeRoadsUtil.CRIME_RADIUS
+      )
+        roadCrimes.push(crime);
     }
   });
 
-  roads[i]["crimes"] = roadCrimes;
+  roads[i]["crimesIds"] = roadCrimes.map(crime => crime.id);
+  const crimeRating = crimeRoadsUtil.calcCrimeRating(roadCrimes);
+  roads[i]["totalCrimeRating"] = crimeRating;
+  if (crimeRating) crimeTotal += crimeRating;
 });
 
-fs.writeFile(processedRoadsExportPath, JSON.stringify(roads), function(
+fs.writeFile(processedRoadsExportPath, JSON.stringify(roads), function(err) {
   err
-) {
-  err ? console.log(err) : console.log("Processed Roads data was sucessfully exported!");
+    ? console.log(err)
+    : console.log("Processed Roads data was sucessfully exported!");
 });
+
+console.log(`Average crime rating: ${crimeTotal / roads.length}`);
