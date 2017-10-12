5579574076e9c71ff4137e5c21a5fc1977c8b967
Alvin Kong <alvink92@gmail.com>
2017-10-11 17:09:10 -0700

    refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,16 +23,21 @@ end
 
 roadsFile = File.read(File.expand_path('road_network_data/data/processed/roads.json'))
 roads = JSON.parse(roadsFile)
-
+# puts roads[0]
+# puts roads.length
 roads.each do |road|
   begin
+    total_crime_rating = road["totalCrimeRating"] ? road["totalCrimeRating"] : 0
     Road.create(id: road["id"], intersection1_id: road["intersection1_id"],
       intersection2_id: road["intersection2_id"],
       street_name: road["street_name"],
       nearby_crimes_ids: road["crimeIds"],
-      total_crime_rating: road["totalCrimeRating"],
+      total_crime_rating: total_crime_rating,
       distance: road["distance"])
   rescue
-    # puts road
+    # puts "-----------------"
+    # puts road["id"]
+    # puts road["intersection1_id"]
+    # puts road["intersection2_id"]
   end
 end
