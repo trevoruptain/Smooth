5ffec362cd957dde4ea8f8c1ac764a6635102a16
Dillon <dillontluther@gmail.com>
2017-10-13 20:47:53 -0700

    fetch is alerting latitude from response

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGNodeList.d
@@ -1,6 +1,3 @@
 dependencies: \
-  /Users/trevoruptain/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
-  /Users/trevoruptain/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/trevoruptain/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/trevoruptain/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/trevoruptain/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/lexluther/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/lexluther/Desktop/Smooth/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h
